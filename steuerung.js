edb5accb84a0cc229ec2af9b6b089c89e58ba7ca
wunscha <richhard.wansch@gmail.com>
2022-12-06 20:07:27 +0100

    steuerungDurchNeigung und logging

--- steuerung.js
+++ steuerung.js
@@ -2,6 +2,7 @@
  * Crafty Component, der Steuerung eines Elements durch verschiedene Methoden (Neigung, Maus, etc) ermöglicht
  */
 Crafty.c('Steuerung', {
+    init: function() {console.log('Component Steuerung hinzugefügt')},
     steuerungDurchNeigung: steuerungDurchNeigung,
 })
 
@@ -12,15 +13,45 @@ Crafty.c('Steuerung', {
  */
 function steuerungDurchNeigung(mapAttribut, mapDaempfung) {
     console.log('steuerungDurchNeigung hinzugefügt zu:', this);
-    document.addEventListener('deviceorientation', evt => {
+    hinweiseMapAttribut(mapAttribut);
+    window.addEventListener('deviceorientation', evt => {
         if (mapAttribut.alpha) {
-            this[mapAttribut.alpha] = evt.alpha * mapDaempfung.alpha;
+            console.log('alpha:', evt.alpha, '; this.rotation:', this.rotation);
+            this[mapAttribut.alpha] = 
+                mapDaempfung && mapDaempfung.alpha ?
+                evt.alpha * mapDaempfung.alpha:
+                evt.alpha;
         }
         if (mapAttribut.beta) {
-            this[mapAttribut.beta] = evt.beta * mapDaempfung.beta;
+            this[mapAttribut.beta] = 
+                mapDaempfung && mapDaempfung.beta ?
+                evt.beta * mapDaempfung.beta:
+                evt.beta;
         }
         if (mapAttribut.gamma) {
-            this[mapAttribut.gamma] = evt.gamma * mapDaempfung.gamma
+            this[mapAttribut.gamma] = 
+                mapDaempfung && mapDaempfung.gamma ?
+                evt.gamma * mapDaempfung.gamma:
+                evt.gamma;
         }
     })
+    return this
+}
+
+/**
+ * Warhinweise 
+ * @param {*} mapAttribut 
+ */
+function hinweiseMapAttribut(mapAttribut) {
+    const valuesMapAttribut = Object.values(mapAttribut);
+    if (valuesMapAttribut.includes('rotation')){
+        console.log('rotation wird gesteuert --> origin() nicht vergessen')
+    }
+    if (
+        valuesMapAttribut.includes('vx') ||
+        valuesMapAttribut.includes('vy') ||
+        valuesMapAttribut.includes('vy')
+    ){
+        console.log('v wird gesteuert --> Motion-Component nicht vergessen')
+    }
 }
